74c9ab6cf625ae3d9bc9f5093da3fffe5ebad3e9
fasape <mstepten@163.com>
2018-11-27 22:31:36 +0800

    syn

--- src/main/java/cn/edu/ruc/utils/GenerateData.java
+++ src/main/java/cn/edu/ruc/utils/GenerateData.java
@@ -1,6 +1,8 @@
 package cn.edu.ruc.utils;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -14,13 +16,20 @@ import java.util.TreeMap;
 
 public class GenerateData {
 	private static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+	private static double INTERNAL=7;
     public static void main(String[] args) throws Exception {
-    	String startDateStr="2018-01-01T00:00:00.000";
-    	String endDateStr="2018-01-08T00:00:00.000";
-		long startTime = DATE_FORMAT.parse(startDateStr).getTime();
+        Options options=new Options();
+        Option pathOption = Option.builder("path").build();
+        Option internalOption = Option.builder("d").build();
+
+        String startDateStr="2018-01-01T00:00:00.000";
+        long startTime = DATE_FORMAT.parse(startDateStr).getTime();
+        if(args.length==1){
+
+        }
+        String endDateStr="2018-01-08T00:00:00.000";
 		long currentTime= startTime;
 		long endTime=DATE_FORMAT.parse(endDateStr).getTime();
-		
 		Random r =new Random();
 		System.out.println(startTime);
 		System.out.println(endTime);
@@ -45,20 +54,13 @@ public class GenerateData {
 //					System.out.println(JSON.toJSON(map));
 				}
 			}
-			appendFile(sc.toString(),"E:\\ac.txt");
+			appendFile(sc.toString(),"ac.txt");
 			currentTime+=7000;
 		}
-		String str="";
-		
-//		Date date = new Date(startTime);
-//		String format = DATE_FORMAT.format(date);
-//		System.out.println(format);
 	}
 	private static void appendFile(String data,String path) {
 		FileWriter fw = null;
 		try {
-		//如果文件存在，则追加内容；如果文件不存在，则创建文件
-//			File f=new File("E:\\dd.txt");
 			File f=new File(path);
 			fw = new FileWriter(f, true);
 		} catch (Exception e) {
