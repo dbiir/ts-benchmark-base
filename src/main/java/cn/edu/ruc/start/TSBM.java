6caec2808fd37bbf452090b8c5e34ce229e079ab
fasape <mstepten@163.com>
2019-06-10 20:43:31 +0800

    1

--- src/main/java/cn/edu/ruc/start/TSBM.java
+++ src/main/java/cn/edu/ruc/start/TSBM.java
@@ -78,7 +78,7 @@ public class TSBM {
             System.out.println("<<<<<<<<<<generate data finished " + System.currentTimeMillis() + "<<<<<<<");
         }else{
             System.out.println(">>>>>>>>>>generate insert data begin " + System.currentTimeMillis() + ">>>>>>>>>>");
-//            generateInsertData(dataPath, maxFarm, maxRows);
+            generateInsertData(dataPath, maxFarm, maxRows);
             System.out.println("<<<<<<<<<<generate insert data finished " + System.currentTimeMillis() + "<<<<<<<");
         }
 
